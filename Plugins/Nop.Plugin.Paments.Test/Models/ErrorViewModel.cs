d6837e093b0f4592bc66267901a39b2828fa13e0
JadhAka <akash.jadhav@sts.in>
2022-08-09 21:06:02 +0530

    Removed unwanted Plugin

--- Plugins/Nop.Plugin.Paments.Test/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Nop.Plugin.Paments.Test.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
